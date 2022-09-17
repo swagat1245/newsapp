df68576c1c8f3f81b857953d3f9ea7d428a447ce
swagat <swagatpatil585@gmail.com>
2022-09-17 11:58:17 +0530

    hi

--- src/component/News.js
+++ src/component/News.js
@@ -7,17 +7,43 @@ export class News extends Component {
     super();
     this.state = {
       articles: [],
-      loading: false
+      loading: false,
+      page: 1
     }
 
   }
-  async componentDidMount(){
-    let url = "https://newsapi.org/v2/top-headlines?country=in&apiKey=d637576a17c540abab8f1d5392116aa1";
+  async componentDidMount() {
+    let url = "https://newsapi.org/v2/top-headlines?country=in&apiKey=d637576a17c540abab8f1d5392116aa1&pageSize=20";
     let data = await fetch(url);
     let parseData = await data.json();
-    this.setState({articles:parseData.articles})
+    this.setState({ articles: parseData.articles, totalResults: parseData.totalResults })
 
   }
+  handlePrevClick = async () => {
+    console.log("previous");
+    let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=d637576a17c540abab8f1d5392116aa1&page=${this.state.page - 1}&pageSize=20`;
+    let data = await fetch(url);
+    let parseData = await data.json();
+    this.setState({
+      page: this.state.page - 1,
+      articles: parseData.articles
+    })
+  }
+  handleNextClick = async () => {
+    console.log("next");
+    if (this.state.page + 1 > Math.ceil(this.state.totalResults / 20)) {
+
+    } else {
+      let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=d637576a17c540abab8f1d5392116aa1&page=${this.state.page + 1}&pageSize=20`;
+      let data = await fetch(url);
+      let parseData = await data.json();
+      this.setState({
+        page: this.state.page + 1,
+        articles: parseData.articles
+      })
+    }
+  }
+
   render() {
     return (
       <div className="container my-3">
@@ -25,9 +51,15 @@ export class News extends Component {
         <div className="row">
           {this.state.articles.map((element) => {
             return <div className="col-md-4" key={element.url}>
-              <NewsItem title={element.title?element.title.slice(0,45):""} descripton={element.description?element.description.slice(0,88):""} imageUrl={element.urlToImage} newsUrl={element.url}/>
+              <NewsItem title={element.title ? element.title.slice(0, 45) : ""} descripton={element.description ? element.description.slice(0, 88) : ""} imageUrl={element.urlToImage} newsUrl={element.url} />
             </div>
           })}
+
+          <div className="container d-flex justify-content-between">
+            <button disabled={this.state.page <= 1} type="button" className="btn btn-dark" onClick={this.handlePrevClick}> &larr; Previous</button>
+            <button type="button" className="btn btn-dark" onClick={this.handleNextClick}>Next &rarr;</button>
+          </div>
+
         </div>
       </div>
 
