7ed4dd15cf583a9675b85e5a32f615e57f3b0cec
swagat <swagatpatil585@gmail.com>
2022-09-23 10:16:57 +0530

    final commit

--- src/component/News.js
+++ src/component/News.js
@@ -4,73 +4,59 @@ import Spinner from './Spinner';
 import InfiniteScroll from "react-infinite-scroll-component";
 import PropTypes from 'prop-types'
 const News = (props) => {
-    const [articles, setArticles] = useState([])
-    const [loading, setLoading] = useState(true)
-    const [page, setPage] = useState(1)
-    const [totalResults, setTotalResult] = useState(0)
-    // document.title = props.category;
-
-  
-  const  updateNews = async()=> {
+  const [articles, setArticles] = useState([])
+  const [loading, setLoading] = useState(true)
+  const [page, setPage] = useState(1)
+  const [totalResults, setTotalResult] = useState(0)
+  const updateNews = async () => {
     props.setProgress(10);
     const url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=${props.apiKey}&page=${page}&pageSize=${props.pageSize}`;
-setLoading(true);
-    props.setProgress(30);
+    setLoading(true);
     let data = await fetch(url);
-    props.setProgress(70);
+    props.setProgress(30);
     let parseData = await data.json();
+    props.setProgress(70);
     setArticles(parseData.articles)
     setTotalResult(parseData.totalResults)
     setLoading(false)
     props.setProgress(100);
   }
   useEffect(() => {
+    document.title = 'props.category - NewsMonkey';
     updateNews();
+    // eslint-disable-next-line
   }, [])
-  
-  const handlePrevClick = async () => {
-    setPage(page-1)
-    updateNews();
-  }
- const handleNextClick = async () => {
-  setPage(page+1)
-  updateNews();      
-    }
-  
   const fetchMoreData = async () => {
-    setPage(page+1)
-    const url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=${props.apiKey}&page=${page}&pageSize=${props.pageSize}`;  
+    const url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=${props.apiKey}&page=${page + 1}&pageSize=${props.pageSize}`;
+    setPage(page + 1)
     let data = await fetch(url);
     let parseData = await data.json();
     setArticles(articles.concat(parseData.articles))
     setTotalResult(parseData.totalResults)
   };
-
-    return (
-      <>
-        <h1 className="text-center" sttyle={{ margin: "35px" }}>NewsMonkey - Top props.category Headlines</h1>
-        {loading && <Spinner />}
-        <InfiniteScroll
-          dataLength={articles.length}
-          next={fetchMoreData}
-          hasMore={articles.length !== totalResults}
-          loader={<Spinner />}
-        >
-          <div className="container">
-            <div className="row">
-              {articles.map((element) => {
-                return <div className="col-md-4" key={element.url}>
-                  <NewsItem title={element.title ? element.title.slice(0, 45) : ""} descripton={element.description ? element.description.slice(0, 88) : ""} imageUrl={element.urlToImage} newsUrl={element.url} author={element.author} date={element.publishedAt} source={element.source.name} />
-                </div>
-              })}
-            </div>
+  return (
+    <>
+      <h1 className="text-center" sttyle={{ margin: "35px", marginTop: '90px' }}>NewsMonkey - Top props.category Headlines</h1>
+      {loading && <Spinner/>}
+      <InfiniteScroll
+        dataLength={articles.length}
+        next={fetchMoreData}
+        hasMore={articles.length !== totalResults}
+        loader={<Spinner/>}
+      >
+        <div className="container">
+          <div className="row">
+            {articles.map((element) => {
+              return <div className="col-md-4" key={element.url}>
+                <NewsItem title={element.title ? element.title.slice(0, 45) : ""} descripton={element.description ? element.description.slice(0, 88) : ""} imageUrl={element.urlToImage} newsUrl={element.url} author={element.author} date={element.publishedAt} source={element.source.name} />
+              </div>
+            })}
           </div>
-        </InfiniteScroll>
+        </div>
+      </InfiniteScroll>
+    </>
+  )
 
-
-      </>
-    )
-  
 }
 
 News.defaultProps = {
